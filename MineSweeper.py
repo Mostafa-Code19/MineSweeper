005498a9f6e35631f92043557dcbc16c249c2450
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-02-23 09:53:05 +0330

    added function: show the unknown position

--- MineSweeper.py
+++ MineSweeper.py
@@ -1,12 +1,12 @@
 def showTheRows(round):
-    rows = round.split('|')
+    rows = round.split(' ')
     numberOfRows = len(rows)
     for row in range(numberOfRows):
         print(rows[row])
     return 'Done'
 
 def countTheBombs(round):
-    rows = round.split('|')
+    rows = round.split(' ')
     bombs = []
     for i in range(len(rows)):
         bombsOfEachRow = 0
@@ -18,11 +18,26 @@ def countTheBombs(round):
     print(bombs)
     return 'Done'
 
+def countTheunknown(round):
+    rows = round.split(' ')
+    unknown = []
+    for i in range(len(rows)):
+        unknownForEachrow = 0
+        for j in range(3):
+            eachElement = rows[i][j]
+            if eachElement == '?':
+                unknownForEachrow += 1
+        unknown.append(unknownForEachrow)
+    print(unknown)
+    return 'Done'
+
 def resolveTheUnknown(round):
     return
 
 try: 
-    print(countTheBombs('*2*|32*'))
+    # print(countTheBombs('121 *?* 121'))
+    # print(showTheRows('121 *?* 121'))
+    print(countTheunknown('121 *?* 121'))
 
 except Exception as e:
     print(e)
