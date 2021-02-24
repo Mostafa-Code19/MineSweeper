0dce897a8a1eb4387462ed909c9f8225597669cb
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-02-24 16:10:04 +0330

    give up the code for next time - Feb 24th 2021

--- MineSweeper.py
+++ MineSweeper.py
@@ -73,9 +73,30 @@ def positionOfTheElements(round, find):                   # 2 * ?    ? 3 *    * 
 def solution(answer):
     return f'? = {answer}' 
 
+def readAllaroundTheElement(game):
+    rows = game.split(' ')
+    rows2 = '123456789'
+    # 2:3 => 12 13 22 32 33 = -11 -10 -1 +9 10
+    # 2:0 => 10 11 20 21 = -10 -9 +1
+    # 1:1 => 01 02 03 10 11 12 20 21 22 = -10 -9 -8 -1 +9 +10 +11
+    # 4 => 0 1 2 3 4 5 6 8 = -4 -3 -2 -1 +1 +2 +3 +4
+    # 3 => 0 1 4 6 7 => -3 -2 +1 +3 +4
+    # print(rows[2 - 1][3 - 1])
+    moves = [-4, -3, -2, -1, +1, +2, +3, +4]
+    for i in (moves):
+        try:
+            place = 3 + i
+            print(rows2[place])
+        except:
+            pass
+    return 
 
 
-print(positionOfTheElements('*6* 3?3 1*2', '?'))
+
+
+# print(positionOfTheElements('*6* 3?3 1*2', '?'))
+print(readAllaroundTheElement('*6* 3?3 1*2'))
+
 # try: 
 #     # print(countTheBombs('121 *?* 121'))
 #     # print(showTheRows('121 *?* 121'))
