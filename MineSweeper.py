72c3f58819e6d3597bfdd77e614576cdf44b72ce
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-02-25 13:17:41 +0330

    all corner function added + all 1 to 3 analyse completed

--- MineSweeper.py
+++ MineSweeper.py
@@ -1,62 +1,87 @@
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format='log: %(message)s')
-logging.disable(logging.CRITICAL)
+# logging.disable(logging.CRITICAL)
 def log(code):
     logging.debug(code)
 
 
+playgroundBefore = ['2?1', '?21', '11S']
 # playground = ['122', '?*?', '?2?']  # = 122 S** S2S
 # playground = ['?3*', '*6*', '***']
-# playground = ['221', '?*?', '???']
-# playground = ['2?1', '?21', '11S']
-playgroundBefore = ['3?2', '??2', '221']
-playground = ['3?2', '??2', '221']
+# playground = ['221', '?*?', '???']  # 221 **S SSS
+playground = ['2?1', '?21', '11S']
+# playground = ['3?2', '2?2', '221']
+# playground = ['1?2', '12*', '1?2']
+# playground = ['1?1', '222', '1?1']
 
 
 x = 3
 y = 3
 playgroundsize = [x, y]
 
-def corner(row, column):
-    log('enter corner')
-    if row == 0 and column == 0: return 'topLeft'
-    elif row == 0 and column == x: return 'upRight'
-    elif row == x and column == 0: return 'downLeft'
-    elif row == x and column == y: return 'downRight'
-    else: return False
+def solve():
+    for element in range(1, 2):
+        for row in range(y):
+            for column in range(x):
+                element = playground[row][column]
+                log(f'element:{element}, row:{row}, column:{column}')
+                AnalyzeAround(element, row, column)
+    
+    log('going to end')
+    return solved()
 
 def AnalyzeAround(element, row, column):
-    log('enter check')        
+    log(f'enter AnalyzeAround with element: {element}, row: {row}, column: {column}')        
     elementAround = []
     whichCorner = corner(row, column)
-    log(f'whichCorner: {whichCorner}')
+    log(f'Whichcorner: {whichCorner}')
+    
     if whichCorner != False:
-        log(f'corner: TRUE')
-        if whichCorner == 'topLeft':
-            for row in range(2):
-                for column in range(2):
-                    elementAround.append(playground[row][column])
-            checkAround(element, elementAround, whichCorner)
-    return
+        for row in range(2):
+            for column in range(2):
+                cornerType = {
+                    'TopLeft' : playground[row][column],
+                    'TopRight' : playground[row][x - column - 1],
+                    'BottomLeft' : playground[y - row - 1][column],
+                    'BottomRight' : playground[y - row - 1][x - column - 1]
+                }
+                cornerApply = cornerType[whichCorner]
+                elementAround.append(cornerApply)
+        
+        log(f'elementAround: {elementAround}')
+        checkAround(element, elementAround, whichCorner)
+
+    else:
+        print('Not Corner')\
+
+def corner(row, column):
+    log('enter corner')
+    if row == 0 and column == 0: return 'TopLeft'
+    elif row == 0 and column == x - 1: return 'TopRight'
+    elif row == x - 1 and column == 0: return 'BottomLeft'
+    elif row == x - 1 and column == y - 1: return 'BottomRight'
+    else: return False
 
 def checkAround(element, elementAround, where):
-    log('enter checkAround')
+    log('67: enter checkAround')
     unknownCounter = elementAround.count('?')
-    unknownIndex = (elementAround.index('?'))
+    if not unknownCounter:
+        return
+    unknownIndex = elementAround.index('?')
     bombCounter = elementAround.count('*')
-    log(f'number of ?: {unknownCounter} | number of *: {bombCounter} | Index of ?: {unknownIndex}')
+    log(f'number of ?: {unknownCounter} |  Index of ?: {unknownIndex} | number of *: {bombCounter}')
 
     if element == '1':
-        if unknownCounter == 1 and bombCounter == 0 in elementAround:
+        if unknownCounter == 1 and bombCounter == 0:
             addSolution(where, unknownCounter, solution = '*')
-        elif '*' in elementAround:
+        elif bombCounter:
             addSolution(where, unknownCounter, solution = 'S')
         else:
-            log('There is an error in checkAround')
+            log('There is an error in checkAround !!!!!!!!!!!!!!!!')
 
     elif element == '2':
-        if unknownCounter == 1 and bombCounter == 1:
+        if unknownCounter and bombCounter:
             addSolution(where, unknownCounter, solution = '*')
         elif unknownCounter == 2:
             addSolution(where, unknownCounter, solution = '*')
@@ -66,39 +91,51 @@ def checkAround(element, elementAround, where):
             log('There is an error in checkAround')
 
     elif element == '3':
-        log(f'Entering element 3')
         if unknownCounter == 3 and bombCounter == 0:
             addSolution(where, 8, solution = '*')
-        elif unknownCounter == 2 and bombCounter == 1:
+        elif unknownCounter == 2 and bombCounter:
             addSolution(where, unknownCounter, solution = '*')
-        elif unknownCounter == 1 and bombCounter == 2:
+        elif unknownCounter and bombCounter == 2:
             addSolution(where, unknownCounter, solution = '*')
         elif bombCounter == 3:
             addSolution(where, 8, solution = 'S')
         else:
             log('There is an error in checkAround')
-        log('going to end')
-        return solved()
+
     else: return 'The element bigger than 3'
 
 def addSolution(where, unknownToAdd, solution):
-    if where == 'topLeft':
+    if where == 'TopLeft':
         for row in range(2):
             for column in range(2):
                 if playground[row][column] == '?':
-                    x = playground[row].replace('?', solution, unknownToAdd)
-                    playground[row] = x
-    return
+                    newRow = playground[row].replace('?', solution, unknownToAdd)
+                    playground[row] = newRow
 
-def solve():
-    for element in range(1, 2):
-        log(element)
-        for row in range(y):
-            for column in range(x):
-                element = playground[row][column]
-                log(f'element:{element}, row:{row}, column:{column}')
-                AnalyzeAround(element, row, column)
-    return ' '
+    elif where == 'TopRight':
+        for row in range(2):
+            for column in range(2):
+                if playground[row][x - column - 1] == '?':
+                    newRow = playground[row].replace('?', solution, unknownToAdd)
+                    playground[row] = newRow
+        log(f'115: newRow: {newRow}')
+
+    elif where == 'BottomLeft':
+        for row in range(2):
+            for column in range(2):
+                if playground[y - row - 1][column] == '?':
+                    newRow = playground[y - row - 1].replace('?', solution, unknownToAdd)
+                    playground[y - row - 1] = newRow
+        log(f'137: newRow: {newRow}')
+
+    elif where == 'BottomRight':
+        for row in range(2):
+            for column in range(2):
+                if playground[y - row - 1][x - column - 1] == '?':
+                    newRow = playground[y - row - 1].replace('?', solution, unknownToAdd)
+                    playground[y - row - 1] = newRow
+        log(f'152: newRow: {newRow}')
+    return
 
 def solved():
     rows = len(playground)
