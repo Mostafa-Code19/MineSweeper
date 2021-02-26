1aa6493093558ef25639c4504cc48b5db9f7a6c4
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-02-26 06:59:51 +0330

    DEBUG: index of TRBL : line-61

--- MineSweeper.py
+++ MineSweeper.py
@@ -6,27 +6,27 @@ def log(code):
     logging.debug(code)
 
 
-playgroundBefore = ['2?1', '?21', '11S']
+playgroundBefore = ['1?1', '222', '1?1']
 # playground = ['122', '?*?', '?2?']  # = 122 S** S2S
 # playground = ['?3*', '*6*', '***']
 # playground = ['221', '?*?', '???']  # 221 **S SSS
-playground = ['2?1', '?21', '11S']
-# playground = ['3?2', '2?2', '221']
+# playground = ['2?1', '?21', '11*']
+# playground = ['3?2', '??2', '221']
 # playground = ['1?2', '12*', '1?2']
-# playground = ['1?1', '222', '1?1']
-
+playground = ['1?1', '222', '1?1']
 
 x = 3
 y = 3
 playgroundsize = [x, y]
 
 def solve():
-    for element in range(1, 2):
-        for row in range(y):
-            for column in range(x):
-                element = playground[row][column]
-                log(f'element:{element}, row:{row}, column:{column}')
-                AnalyzeAround(element, row, column)
+    for repeatTheCheck in range(round(len(playground) / 2)) :
+        for element in range(1, 2):
+            for row in range(y):
+                for column in range(x):
+                    element = playground[row][column]
+                    log(f'element:{element}, row:{row}, column:{column}')
+                    AnalyzeAround(element, row, column)
     
     log('going to end')
     return solved()
@@ -34,26 +34,39 @@ def solve():
 def AnalyzeAround(element, row, column):
     log(f'enter AnalyzeAround with element: {element}, row: {row}, column: {column}')        
     elementAround = []
-    whichCorner = corner(row, column)
-    log(f'Whichcorner: {whichCorner}')
-    
-    if whichCorner != False:
-        for row in range(2):
-            for column in range(2):
+    if corner(row, column):
+        positionType = corner(row, column)
+        log(f'positionType: {positionType} {row} {column}')
+        for moveY in range(2):
+            for moveX in range(2):
                 cornerType = {
-                    'TopLeft' : playground[row][column],
-                    'TopRight' : playground[row][x - column - 1],
-                    'BottomLeft' : playground[y - row - 1][column],
-                    'BottomRight' : playground[y - row - 1][x - column - 1]
+                    'TopLeft' : playground[moveX][moveY],
+                    'TopRight' : playground[moveX][x - moveY - 1],
+                    'BottomLeft' : playground[y - moveX - 1][moveY],
+                    'BottomRight' : playground[y - moveX - 1][x - moveY - 1]
                 }
-                cornerApply = cornerType[whichCorner]
+                cornerApply = cornerType[positionType]
                 elementAround.append(cornerApply)
         
         log(f'elementAround: {elementAround}')
-        checkAround(element, elementAround, whichCorner)
+        checkAround(element, elementAround, positionType)
 
-    else:
-        print('Not Corner')\
+    elif TRBL(row, column):
+        positionType = TRBL(row, column)
+        for move in range(2):
+            for moveOpposite in (1, 0, -1):
+                log(f'positionType: {positionType} {row} {column} {move} {moveOpposite}')
+                TRBLType = {
+                    'Top' : playground[row + move][column - moveOpposite], # 01 | 00 01 02 10 11 12 ok
+                    'Right' : playground[row - moveOpposite][column - move], # 12 | 02 12 22 01 11 21
+                    'Bottom' : playground[row - move][column - moveOpposite], # 21 | 20 21 22 10
+                    'Left' : playground[row - moveOpposite][column + move], # 10 | 00 10 20 01 11 21 ok
+                }
+                cornerApply = TRBLType[positionType]
+                elementAround.append(cornerApply)
+        
+        log(f'elementAround: {elementAround}')
+        checkAround(element, elementAround, positionType)
 
 def corner(row, column):
     log('enter corner')
@@ -61,6 +74,14 @@ def corner(row, column):
     elif row == 0 and column == x - 1: return 'TopRight'
     elif row == x - 1 and column == 0: return 'BottomLeft'
     elif row == x - 1 and column == y - 1: return 'BottomRight'
+    else: return False
+
+def TRBL(row, column):
+    log('enter TRBL')
+    if column == 0: return 'Left'
+    elif column == x: return 'Right'
+    elif row == 0: return 'Top'
+    elif row == y: return 'Bottom'
     else: return False
 
 def checkAround(element, elementAround, where):
